0734820389300637ffb92c5a1d7508f6d51fba9d
a21jeaha <hisui86@gmail.com>
2026-02-06 12:38:48 +0100

    Create CrawlerSettingsDTO.cs

---
+++ LTU.SearchEngine.Backend/LTU.SearchEngine.Backend.Core/Model/CrawlerSettingsDTO.cs
@@ -0,0 +1,22 @@
+﻿namespace LTU.SearchEngine.Backend.Core.Model;
+
+public class CrawlerSettingsDTO
+{
+	public string UserAgent { get; }
+	public int MaxConcurrencyPerDomain { get; }
+	public int MinDelayMs { get; }
+	public IReadOnlyList<TimeSpan> RetryIntervals { get; }
+
+	public CrawlerSettingsDTO(
+		string userAgent,
+		int maxConcurrencyPerDomain,
+		int minDelayMs,
+		IReadOnlyList<TimeSpan> retryIntervals
+		)
+	{
+		UserAgent = userAgent;
+		MaxConcurrencyPerDomain = maxConcurrencyPerDomain;
+		MinDelayMs = minDelayMs;
+		RetryIntervals = retryIntervals;
+	}
+}
