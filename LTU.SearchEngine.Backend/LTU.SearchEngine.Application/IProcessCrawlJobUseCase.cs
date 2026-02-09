a313da4654297e40c350f37886901d8162fa868d
a21jeaha <hisui86@gmail.com>
2026-02-09 14:49:40 +0100

    Added interfaces for class dependencies

---
+++ LTU.SearchEngine.Backend/LTU.SearchEngine.Application/IProcessCrawlJobUseCase.cs
@@ -0,0 +1,9 @@
+﻿using LTU.SearchEngine.Backend.Core.Model.Entities;
+using LTU.SearchEngine.Backend.Core.Model.ValueObjects;
+
+namespace LTU.SearchEngine.Application;
+
+public interface IProcessCrawlJobUseCase
+{
+	public Task<CrawlResult> Execute(CrawlJob job);  
+}
