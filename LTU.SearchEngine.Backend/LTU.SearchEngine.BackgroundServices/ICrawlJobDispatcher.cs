bbd9f18ad303cb313f37c4a595fb09126c43f80c
a21jeaha <hisui86@gmail.com>
2026-02-11 13:55:55 +0100

    added the ICrawlJobDispatcher and its empty implementation

---
+++ LTU.SearchEngine.Backend/LTU.SearchEngine.BackgroundServices/ICrawlJobDispatcher.cs
@@ -0,0 +1,9 @@
+﻿using LTU.SearchEngine.Backend.Core.Model.Entities;
+
+namespace LTU.SearchEngine.BackgroundServices;
+
+public interface ICrawlJobDispatcher
+{
+	public Task Enqueue(CrawlJob job);
+	public Task Start(CancellationToken ct);
+}
---
+++ LTU.SearchEngine.Backend/LTU.SearchEngine.BackgroundServices/TplCrawlJobDispatcher.cs
@@ -0,0 +1,16 @@
+﻿using LTU.SearchEngine.Backend.Core.Model.Entities;
+
+namespace LTU.SearchEngine.BackgroundServices;
+
+public class TplCrawlJobDispatcher : ICrawlJobDispatcher
+{
+	public Task Enqueue(CrawlJob job)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task Start(CancellationToken ct)
+	{
+		throw new NotImplementedException();
+	}
+}
