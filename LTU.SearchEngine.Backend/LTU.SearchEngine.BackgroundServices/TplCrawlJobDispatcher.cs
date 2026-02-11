f8a6b6bdce7ef8139b4f1dd40a77060e99811e18
a21jeaha <hisui86@gmail.com>
2026-02-11 16:37:23 +0100

    Added the needed fields and constructor

--- LTU.SearchEngine.Backend/LTU.SearchEngine.BackgroundServices/TplCrawlJobDispatcher.cs
+++ LTU.SearchEngine.Backend/LTU.SearchEngine.BackgroundServices/TplCrawlJobDispatcher.cs
@@ -1,9 +1,36 @@
-﻿using LTU.SearchEngine.Backend.Core.Model.Entities;
+﻿using LTU.SearchEngine.Application;
+using LTU.SearchEngine.Backend.Core.Model.Entities;
+using LTU.SearchEngine.Backend.Core.Model.ValueObjects;
+using System.Threading.Tasks.Dataflow;
 
 namespace LTU.SearchEngine.BackgroundServices;
 
 public class TplCrawlJobDispatcher : ICrawlJobDispatcher
 {
+	private readonly IProcessCrawlJobUseCase _processCrawlJobUseCase;
+	private readonly CrawlerSettings _crawlerSettings;
+	private PriorityQueue<CrawlJob, DateTime> _jobPriorityQueue;
+	private BufferBlock<CrawlJob> _buffer;
+	private ActionBlock<CrawlJob> _worker;
+
+	public TplCrawlJobDispatcher(
+		IProcessCrawlJobUseCase processCrawlJobUseCase,
+		CrawlerSettings crawlerSettings
+		)
+	{
+		_processCrawlJobUseCase = processCrawlJobUseCase 
+			?? throw new ArgumentNullException(nameof(processCrawlJobUseCase));
+		_crawlerSettings = crawlerSettings 
+			?? throw new ArgumentNullException(nameof(crawlerSettings));
+
+		_jobPriorityQueue = new PriorityQueue<CrawlJob, DateTime>();
+		_buffer = new BufferBlock<CrawlJob>();
+		_worker = new ActionBlock<CrawlJob>(async job =>
+		{
+			await _processCrawlJobUseCase.Execute(job);
+		});
+	}
+
 	public Task Enqueue(CrawlJob job)
 	{
 		throw new NotImplementedException();
