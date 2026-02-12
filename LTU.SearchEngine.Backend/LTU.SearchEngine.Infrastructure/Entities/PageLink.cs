5b9541a1e80437adb2f460c6fb216c22615888c8
Emma <you@example.com>
2026-02-12 16:43:43 +0100

    Added all entities

---
+++ LTU.SearchEngine.Backend/LTU.SearchEngine.Infrastructure/Entities/PageLink.cs
@@ -0,0 +1,20 @@
+﻿using LTU.SearchEngine.Backend.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LTU.SearchEngine.Infrastructure.Entities
+{
+    public class PageLink
+    {
+        // ER: from_page_id integer
+        // Sidan som länken finns PÅ
+        public int FromPageId { get; set; }
+        public Page FromPage { get; set; } // Navigering (EF Core)
+
+        // ER: to_page_id integer
+        // Sidan som länken pekar TILL
+        public int ToPageId { get; set; }
+        public Page ToPage { get; set; } // Navigering (EF Core)
+    }
+}
---
+++ LTU.SearchEngine.Backend/LTU.SearchEngine.Infrastructure/Entities/Term.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LTU.SearchEngine.Infrastructure.Entities
+{
+    public class Term
+    {
+        // ER: id integer (PK)
+        public int Id { get; set; }
+
+        // ER: normalized_word varchar UNIQUE
+        // Detta är det "tvättade" ordet (t.ex. "running" -> "run")
+        public string Word { get; set; } = string.Empty;
+
+        // ER: idf_score float
+        // Inverse Document Frequency - används för att veta hur unikt ordet är.
+        // Vi använder double för matematikens skull i C#.
+        public double IdfScore { get; set; }
+
+        // --- RELATIONER (Dessa aktiverar vi i Subtask 3) ---
+        // ER: Koppling till page_word_frequency
+        // public ICollection<PageWordFrequency> PageFrequencies { get; set; }
+
+    }
+}
