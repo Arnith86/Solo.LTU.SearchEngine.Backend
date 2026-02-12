88b36ea6ccf3dbbd76540fbcdaf985acd727876d
Emma <you@example.com>
2026-02-12 16:16:55 +0100

    created searchdbcontext class

---
+++ LTU.SearchEngine.Backend/LTU.SearchEngine.Infrastructure/SearchDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LTU.SearchEngine.Infrastructure
+{
+    public class SearchDbContext : DbContext
+    {
+        public SearchDbContext(DbContextOptions<SearchDbContext> options)
+    : base(options)
+        {
+        }
+    }
+}
