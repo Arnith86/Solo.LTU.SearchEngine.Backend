91618f1e301929c84b1454405232f527335bb0a9
Emma <you@example.com>
2026-02-16 15:16:38 +0100

    moved setting for entities to their own configurationfile

---
+++ LTU.SearchEngine.Backend/LTU.SearchEngine.Infrastructure/Configurations/PageConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using LTU.SearchEngine.Backend.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LTU.SearchEngine.Infrastructure.Configurations
+{
+    public class PageConfiguration : IEntityTypeConfiguration<Page>
+    {
+        public void Configure(EntityTypeBuilder<Page> builder)
+        {
+            builder.HasIndex(p => p.Url).IsUnique();
+        }
+    }
+}
--- LTU.SearchEngine.Backend/LTU.SearchEngine.Infrastructure/Data/SearchDbContext.cs
+++ LTU.SearchEngine.Backend/LTU.SearchEngine.Infrastructure/Data/SearchDbContext.cs
@@ -19,44 +19,7 @@ namespace LTU.SearchEngine.Infrastructure.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-
-            modelBuilder.Entity<Page>()
-                .HasIndex(p => p.Url)
-                .IsUnique();
-
-            modelBuilder.Entity<Term>()
-                .HasIndex(t => t.Word)
-                .IsUnique();
-
-            modelBuilder.Entity<PageWordFrequency>()
-                .HasKey(pwf => new { pwf.PageId, pwf.TermId });
-
-            modelBuilder.Entity<PageWordFrequency>()
-                .HasOne(pwf => pwf.Page)
-                .WithMany(p => p.WordFrequencies) 
-                .HasForeignKey(pwf => pwf.PageId);
-
-            modelBuilder.Entity<PageWordFrequency>()
-                .HasOne(pwf => pwf.Term)
-                .WithMany(t => t.PageFrequencies) 
-                .HasForeignKey(pwf => pwf.TermId);
-
-            modelBuilder.Entity<PageLink>()
-                .HasKey(pl => new { pl.FromPageId, pl.ToPageId });
-
-
-            modelBuilder.Entity<PageLink>()
-                .HasOne(pl => pl.FromPage)
-                .WithMany(p => p.OutgoingLinks) 
-                .HasForeignKey(pl => pl.FromPageId)
-                .OnDelete(DeleteBehavior.Restrict);
-
-
-            modelBuilder.Entity<PageLink>()
-                .HasOne(pl => pl.ToPage)
-                .WithMany()
-                .HasForeignKey(pl => pl.ToPageId)
-                .OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(SearchDbContext).Assembly);
         }
     }
 }
