52530ab9c0a3bdefe5f543a9071648892419134f
Emma <you@example.com>
2026-02-21 14:42:30 +0100

    created the interface

---
+++ LTU.SearchEngine.Backend/LTU.SearchEngine.Infrastructure/Configurations/IRobotsHandler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LTU.SearchEngine.Infrastructure.Configurations
+{
+    public interface IRobotsHandler
+    {
+        bool IsAllowed(string url);
+    }
+}
