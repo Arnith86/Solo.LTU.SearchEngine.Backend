d9bd33e7cf95e36d66a80ad427aeb5fecfa20103
Emma <you@example.com>
2026-02-23 15:21:52 +0100

    created phraseandtermnormalizer class and itextnormalizer

---
+++ LTU.SearchEngine.Backend/LTU.SearchEngine.Application/QueryParsing/Helpers/ITextNormalizer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LTU.SearchEngine.Application.QueryParsing.Helpers
+{
+    public interface ITextNormalizer<T>
+    {
+        string Normalize(T token);
+    }
+}
