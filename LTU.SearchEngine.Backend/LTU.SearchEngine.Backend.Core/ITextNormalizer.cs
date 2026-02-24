e4eeb2cd1e3f3bdced3df8696affa00c91943442
Emma <you@example.com>
2026-02-24 15:27:32 +0100

    Moved class and updated comments

--- LTU.SearchEngine.Backend/LTU.SearchEngine.Backend.Core/ITextNormalizer.cs
+++ LTU.SearchEngine.Backend/LTU.SearchEngine.Backend.Core/ITextNormalizer.cs
@@ -6,11 +6,10 @@
 /// <typeparam name="T">The type of token to be normalized.</typeparam>
 public interface ITextNormalizer<T>
     {
-        /// <summary>
-        /// Processes and normalizes the content of a given token, such as 
-        /// converting text to lowercase or handling specific character formatting.
-        /// </summary>
-        /// <param name="token">The token object containing the text to normalize.</param>
-        /// <returns>A normalized string representation of the provided token.</returns>
-        string Normalize(T token);
-    }
+    /// <summary>
+    /// Processes and normalizes the provided input.
+    /// </summary>
+    /// <param name="token">The object to normalize.</param>
+    /// <returns>A normalized string representation of the input.</returns>
+    string Normalize(T token);
+}
