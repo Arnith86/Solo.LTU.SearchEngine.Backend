de8e3446fd6657386326d1301d1f378a8e3f4c50
Emma <you@example.com>
2026-02-24 15:11:51 +0100

    moved interface

---
+++ LTU.SearchEngine.Backend/LTU.SearchEngine.Backend.Core/ITextNormalizer.cs
@@ -0,0 +1,16 @@
+﻿namespace LTU.SearchEngine.Backend.Core;
+
+/// <summary>
+/// Defines a contract for normalizing objects into a string representation.
+/// </summary>
+/// <typeparam name="T">The type of token to be normalized.</typeparam>
+public interface ITextNormalizer<T>
+    {
+        /// <summary>
+        /// Processes and normalizes the content of a given token, such as 
+        /// converting text to lowercase or handling specific character formatting.
+        /// </summary>
+        /// <param name="token">The token object containing the text to normalize.</param>
+        /// <returns>A normalized string representation of the provided token.</returns>
+        string Normalize(T token);
+    }
