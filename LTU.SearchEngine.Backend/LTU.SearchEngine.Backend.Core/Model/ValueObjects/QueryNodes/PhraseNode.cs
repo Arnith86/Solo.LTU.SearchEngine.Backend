4fcbdf4406b755aeea0835bae517d1bef46de6c0
a21jeaha <hisui86@gmail.com>
2026-02-26 16:45:00 +0100

    Updated validation in constructor of PhraseNode + Tests added

--- LTU.SearchEngine.Backend/LTU.SearchEngine.Backend.Core/Model/ValueObjects/QueryNodes/PhraseNode.cs
+++ LTU.SearchEngine.Backend/LTU.SearchEngine.Backend.Core/Model/ValueObjects/QueryNodes/PhraseNode.cs
@@ -16,10 +16,20 @@ public class PhraseNode<T> : QueryNode<T>
 	/// <summary>Initializes a new instance of the <see cref="PhraseNode{T}"/> class.</summary>
 	/// <param name="phrase">A list of <see cref="ExtractedQueryToken"/> forming the phrase. Cannot be null.</param>
 	/// <exception cref="ArgumentNullException">Thrown if the provided phrase list is null.</exception>
+	/// <exception cref="ArgumentException">Thrown if the provided phrase list is empty.</exception>
 	public PhraseNode(List<ExtractedQueryToken> phrase)
 	{
-		Phrase = phrase ??
+		ValidatePhrase(phrase);
+		Phrase = phrase;
+	}
+
+	private void ValidatePhrase(List<ExtractedQueryToken> phrase)
+	{
+		if (phrase is null)
 			throw new ArgumentNullException(nameof(phrase), "must have a value.");
+
+		if (phrase.Count < 1)
+			throw new ArgumentException(nameof(phrase), "cannot be empty.");
 	}
 
 	/// <inheritdoc>/>
