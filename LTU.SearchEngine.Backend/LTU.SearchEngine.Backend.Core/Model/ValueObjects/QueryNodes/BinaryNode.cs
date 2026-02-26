319ca192c8f2ec78d968eacb1ed94cd322fc5907
a21jeaha <hisui86@gmail.com>
2026-02-26 15:12:42 +0100

    added and adjusted documentation

--- LTU.SearchEngine.Backend/LTU.SearchEngine.Backend.Core/Model/ValueObjects/QueryNodes/BinaryNode.cs
+++ LTU.SearchEngine.Backend/LTU.SearchEngine.Backend.Core/Model/ValueObjects/QueryNodes/BinaryNode.cs
@@ -1,11 +1,25 @@
 ﻿namespace LTU.SearchEngine.Backend.Core.Model.ValueObjects.QueryNodes;
 
+
+/// <summary>
+/// Represents a binary operator node in the query tree that connects two sub-expressions.
+/// </summary>
+/// <typeparam name="T">The return type produced by a visitor during tree traversal.</typeparam>
+/// <remarks>
+/// This node is a structural component of the AST, used to represent logical operations <br />
+/// such as AND and OR by linking a left and a right <see cref="QueryNode{T}"/>.
+/// </remarks>
 public class BinaryNode<T> : QueryNode<T>
 {
 	public QueryNode<T> LeftNode { get; }
 	public QueryNode<T> RightNode { get; }
 	public BinaryNode<T> OperatorNode { get; }
-	
+
+	/// <summary>Initializes a new instance of the <see cref="BinaryNode{T}"/> class.</summary>
+	/// <param name="leftNode">The left sub-expression. Cannot be null.</param>
+	/// <param name="rightNode">The right sub-expression. Cannot be null.</param>
+	/// <param name="operatorNode">The node representing the logical operator. Cannot be null.</param>
+	/// <exception cref="ArgumentNullException">Thrown if any of the provided nodes are null.</exception>
 	public BinaryNode(
 		QueryNode<T> leftNode,
 		QueryNode<T> rightNode,
@@ -20,6 +34,7 @@ public class BinaryNode<T> : QueryNode<T>
 			throw new ArgumentNullException(nameof(operatorNode));
 	}
 
+	/// <inheritdoc>/>
 	public override T Accept(IQueryVisitor<T> visitor)
 		=> visitor.Visit(this);
 }
--- LTU.SearchEngine.Backend/LTU.SearchEngine.Backend.Core/Model/ValueObjects/QueryNodes/RequiredNode.cs
+++ LTU.SearchEngine.Backend/LTU.SearchEngine.Backend.Core/Model/ValueObjects/QueryNodes/RequiredNode.cs
@@ -1,15 +1,27 @@
 ﻿namespace LTU.SearchEngine.Backend.Core.Model.ValueObjects.QueryNodes;
 
+/// <summary>
+/// Represents a unary operator node in the query tree that marks a sub-expression as required.
+/// </summary>
+/// <typeparam name="T">The return type produced by a visitor during tree traversal.</typeparam>
+/// <remarks>
+/// This node is typically used to represent the mandatory inclusion operator (e.g., '+') <br />
+/// and wraps another <see cref="QueryNode{T}"/> to indicate it must be present in the search results.
+/// </remarks>
 public class RequiredNode<T> : QueryNode<T>
 {
 	public QueryNode<T> Node { get; }
 
+	/// <summary>Initializes a new instance of the <see cref="RequiredNode{T}"/> class.</summary>
+	/// <param name="node">The sub-expression to be marked as required. Cannot be null.</param>
+	/// <exception cref="ArgumentNullException">Thrown if the provided node is null.</exception>
 	public RequiredNode(QueryNode<T> node)
 	{
 		Node = node ?? 
 			throw new ArgumentNullException(nameof(node));
 	}
 
+	/// <inheritdoc>/>
 	public override T Accept(IQueryVisitor<T> visitor)
 		=> visitor.Visit(this);
 }
