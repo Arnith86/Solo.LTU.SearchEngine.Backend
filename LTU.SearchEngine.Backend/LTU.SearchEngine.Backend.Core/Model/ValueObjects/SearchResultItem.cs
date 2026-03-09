9ac57a67697898302a0bb15c6d0e52bfb58263af
a21jeaha <hisui86@gmail.com>
2026-03-09 14:21:24 +0100

    Create SearchResultItem.cs
    A value object containing a single result from the query results

---
+++ LTU.SearchEngine.Backend/LTU.SearchEngine.Backend.Core/Model/ValueObjects/SearchResultItem.cs
@@ -0,0 +1,30 @@
+﻿namespace LTU.SearchEngine.Backend.Core.Model.ValueObjects;
+
+public class SearchResultItem
+{
+	public string Title { get; }
+	public string Url { get; }
+	public string Snippet { get; }
+
+	public SearchResultItem(string title, string url, string snippet)
+	{
+		ValidateAttributes(title, url, snippet);
+
+		Title = title;
+		Url = url;
+		Snippet = snippet;
+	}
+
+	private void ValidateAttributes(string title, string url, string snippet)
+	{
+		ValidateSingleAttribute(title);
+		ValidateSingleAttribute(url);
+		ValidateSingleAttribute(snippet);
+	}
+
+	private void ValidateSingleAttribute(string attribute)
+	{
+		if (String.IsNullOrWhiteSpace(attribute))
+			throw new ArgumentException(nameof(attribute), "must have a value!");
+	}
+}
