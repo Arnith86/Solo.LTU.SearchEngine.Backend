be47f1c92234f492b77f7967bef2da94c34bb246
Jean-Paul Hanna <101112377+Arnith86@users.noreply.github.com>
2026-03-17 16:16:44 +0100

    Added Swashbuckle documentation to controller

--- LTU.SearchEngine.Backend/LTU.SearchEngine.Api/SearchController.cs
+++ LTU.SearchEngine.Backend/LTU.SearchEngine.Api/SearchController.cs
@@ -1,6 +1,7 @@
 ﻿using LTU.SearchEngine.Application;
 using LTU.SearchEngine.Backend.Core.Model.DTOs;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace LTU.SearchEngine.Api
 {
@@ -21,6 +22,12 @@ namespace LTU.SearchEngine.Api
         /// </summary>
         /// <param name="query">The search string to process (e.g., "cats AND dogs").</param>
         [HttpGet]
+        [SwaggerOperation(
+            Summary = "Perform a search on the search engine.",
+            Description = "Retrieves a list of indexed pages based on the provided search query."
+        )]
+        [SwaggerResponse(200, "The search was successful.", typeof(SearchResponseDTO))]
+        [SwaggerResponse(400, "The search query was empty or invalid.")]
         public async Task<ActionResult<SearchResponseDTO>> GetSearchResponses([FromQuery] string query)
         {
             if (string.IsNullOrWhiteSpace(query))
