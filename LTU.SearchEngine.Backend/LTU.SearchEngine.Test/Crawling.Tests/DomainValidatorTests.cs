d32b094e4bf733d46bb2a8e1ed3522dd6cbb1b31
Jean-Paul Hanna <101112377+Arnith86@users.noreply.github.com>
2026-03-19 08:06:08 +0100

    Create DomainValidatorTests.cs
    and added a tests to verify expected behaviur

---
+++ LTU.SearchEngine.Backend/LTU.SearchEngine.Test/Crawling.Tests/DomainValidatorTests.cs
@@ -0,0 +1,48 @@
+using LTU.SearchEngine.Backend.Core.Model.ValueObjects;
+using LTU.SearchEngine.Infrastructure;
+
+namespace LTU.SearchEngine.Test.Crawling;
+
+public class DomainValidatorTests
+{
+    private readonly DomainValidator _sut;
+
+    public DomainValidatorTests()
+    {
+        CrawlerSettings crawlerSettings = new CrawlerSettings
+        (
+            userAgent: "TestAgent",
+            seedUrls: new List<string> { "ltu.se" },
+            whiteList: new List<string> { "ltu.se", "umu.se" },
+            maxConcurrencyPerDomain: 1, 
+            minDelayMs: 0,
+            retryIntervals: new List<TimeSpan> { TimeSpan.FromSeconds(3600)} 
+        );
+
+        _sut = new DomainValidator(crawlerSettings);
+    }
+
+    [Theory]
+    // Allowed
+    [InlineData("https://ltu.se", true)]
+    [InlineData("http://ltu.se", true)]
+    [InlineData("http://www.ltu.se", true)]
+    [InlineData("http://student.ltu.se", true)]
+    [InlineData("http://sub.department.ltu.se", true)]
+    [InlineData("http://LTU.SE", true)]
+    [InlineData("http://umu.SE", true)]
+    [InlineData("http://www.umu.se", true)]
+    // Not allowed
+    [InlineData("http://www.google.com", false)]
+    [InlineData("http://evil-ltu.com", false)]
+    [InlineData("not-an-url", false)]
+    [InlineData("/relative/path", false)]
+    public void IsWhitelisted_ShouldValidateCorrecty(string url, bool expectedResult)
+    {
+        // Act 
+        bool result = _sut.IsWhitelisted(url);
+
+        // Assert
+        Assert.Equal(expectedResult, result);
+    }
+}
