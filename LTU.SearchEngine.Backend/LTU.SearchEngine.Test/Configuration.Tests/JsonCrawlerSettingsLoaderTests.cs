34a045b588232d776f6d3698fee93b6e6b8e26dc
Jean-Paul Hanna <101112377+Arnith86@users.noreply.github.com>
2026-03-19 14:24:45 +0100

    Added a new test to verify hotswappable appsettings

--- LTU.SearchEngine.Backend/LTU.SearchEngine.Test/Configuration.Tests/JsonCrawlerSettingsLoaderTests.cs
+++ LTU.SearchEngine.Backend/LTU.SearchEngine.Test/Configuration.Tests/JsonCrawlerSettingsLoaderTests.cs
@@ -73,6 +73,101 @@ public class JsonCrawlerSettingsLoaderTests
         Assert.Equal(expectedSeedUrls, crawlerSettings.SeedUrls);
     }
 	
+	
+	
+	[Fact]
+	public void Load_HotSwapSettings_ShouldReturnDifferentCrawlerSettings()
+	{
+		// Arrange
+
+		// First version
+		IReadOnlyList<TimeSpan> expectedRetryIntervalsList = new List<TimeSpan>
+		{
+			TimeSpan.FromSeconds(3600),    // 01:00:00		// 1 hour
+			TimeSpan.FromSeconds(86400),   // 1.00:00:00	// 1 day
+			TimeSpan.FromSeconds(604800)   // 7.00:00:00	// 1 week
+		};
+
+ 		var expectedSeedUrls1 = new List<string> { "ltu.se", "www.ltu.se" };
+ 		var expectedWhiteListDomains1 = new List<string> { "ltu.se" };
+		
+		IConfiguration configuration = InMemoryJSONBuildConfiguration.BuildConfiguration(_validJsonConfig);
+		var dto = configuration.GetSection("CrawlerSettings").Get<CrawlerSettingsDTO>();
+
+
+		// Second version 
+		string secondAgent = "secondAgent";
+		int second_maxConcurrencyPerDomain = 1;
+		int second_minDelayMs = 3000;
+		string second_retryIntervals = "[\"02:00:00\", \"2.00:00:00\", \"17.00:00:00\"]";
+		
+		var secondConfig = $$"""
+		{
+			"CrawlerSettings": {
+				"UserAgent": "{{secondAgent}}",
+				"MaxConcurrencyPerDomain": {{second_maxConcurrencyPerDomain}},
+				"MinDelayMs": {{second_minDelayMs}},
+				"RetryIntervals":  {{second_retryIntervals}},
+				"seedUrls": [
+			    "umu.se",
+		        "www.umu.se"
+		    	],
+				"WhiteList":[
+				"umu.se"
+				]
+			}
+		}
+		""";
+
+		IReadOnlyList<TimeSpan> expectedRetryIntervalsList2 = new List<TimeSpan>
+		{
+			TimeSpan.FromHours(2),          // 02:00:00 (2 timmar)
+			TimeSpan.FromDays(2),           // 2.00:00:00 (2 dagar)
+			TimeSpan.FromDays(17)           // 17.00:00:00 (17 dagar)
+		};
+
+		var expectedSeedUrls2 = new List<string> { "umu.se", "www.umu.se" };
+		var expectedWhiteListDomains2 = new List<string> { "umu.se" };
+
+
+		IConfiguration configuration2 = InMemoryJSONBuildConfiguration.BuildConfiguration(secondConfig);
+		var dto2 = configuration2.GetSection("CrawlerSettings").Get<CrawlerSettingsDTO>();
+
+		var mockMonitor = new Mock<IOptionsMonitor<CrawlerSettingsDTO>>();
+		
+		mockMonitor.SetupSequence(m => m.CurrentValue)
+			.Returns(dto!)
+			.Returns(dto2!);
+
+		// var monitor = Options.Create(dto!);
+		
+		ICrawlerSettingsLoader sut = new JsonCrawlerSettingsLoader(mockMonitor.Object);
+				
+
+		// Act
+		CrawlerSettings crawlerSettings1 = sut.Load();
+		CrawlerSettings crawlerSettings2 = sut.Load();     
+
+        // Assert
+		// First
+        Assert.Equal(_c_userAgent, crawlerSettings1.UserAgent);
+		Assert.Equal(_c_maxConcurrencyPerDomain, crawlerSettings1.MaxConcurrencyPerDomain);
+		Assert.Equal(_c_minDelayMs, crawlerSettings1.MinDelayMs);
+		Assert.Equal(expectedRetryIntervalsList, crawlerSettings1.RetryIntervals);
+        Assert.Equal(expectedSeedUrls1, crawlerSettings1.SeedUrls);
+		Assert.Equal(expectedWhiteListDomains1, crawlerSettings1.WhiteList);
+
+		// Second
+		Assert.Equal(secondAgent, crawlerSettings2.UserAgent);
+		Assert.Equal(second_maxConcurrencyPerDomain, crawlerSettings2.MaxConcurrencyPerDomain);
+		Assert.Equal(second_minDelayMs, crawlerSettings2.MinDelayMs);
+		Assert.Equal(expectedRetryIntervalsList2, crawlerSettings2.RetryIntervals);
+        Assert.Equal(expectedSeedUrls2, crawlerSettings2.SeedUrls);
+		Assert.Equal(expectedWhiteListDomains2, crawlerSettings2.WhiteList);
+
+    }
+
+	
 	[Fact]
 	public void Load_InValidConfiguration_ShouldThrow_InvalidOperationException()
 	{
@@ -97,6 +192,4 @@ public class JsonCrawlerSettingsLoaderTests
 		// Act & Assert
 		Assert.Throws<InvalidOperationException>(() => sut.Load());
 	}
-
-
 }
