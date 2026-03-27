6fbd4c8c7c2d783af4ea505df3e7d8bceaeeb385
Jean-Paul Hanna <101112377+Arnith86@users.noreply.github.com>
2026-03-27 08:08:01 +0100

    added documentation to classes

--- LTU.SearchEngine.Backend/LTU.SearchEngine.Backend.Core/HelperClasses/ContentHasher.cs
+++ LTU.SearchEngine.Backend/LTU.SearchEngine.Backend.Core/HelperClasses/ContentHasher.cs
@@ -8,14 +8,7 @@ using System.Security.Cryptography;
 /// </summary>
 public class ContentHasher : IContentHasher
 {
-    /// <summary>
-    /// Computes a SHA256 hash for the specified string content and returns it as a hexadecimal string.
-    /// </summary>
-    /// <param name="content">The string content to be hashed.</param>
-    /// <returns>
-    /// A 64-character hexadecimal string representing the SHA256 hash. <br/>
-    /// Returns <see cref="string.Empty"/> if the input content is null or empty.
-    /// </returns>
+    /// <inheritdoc />
     public string CalculateHash(byte[] content)
     {
         if (content == null || content.Length == 0)
