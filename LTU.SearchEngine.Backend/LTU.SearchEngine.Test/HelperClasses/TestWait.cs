87308bbd9dfbe16d67655e8f15c5224ec6abe768
Jean-Paul Hanna <101112377+Arnith86@users.noreply.github.com>
2026-04-09 14:25:23 +0200

    Added an overload without the func condition

--- LTU.SearchEngine.Backend/LTU.SearchEngine.Test/HelperClasses/TestWait.cs
+++ LTU.SearchEngine.Backend/LTU.SearchEngine.Test/HelperClasses/TestWait.cs
@@ -17,7 +17,25 @@ public static class TestWait
         int totalWait = 0;
         
         // We want to loop AS LONG AS the condition is FALSE and we haven't timed out
-        while (condition() && totalWait < maxWaitMs)
+        while (!condition() && totalWait < maxWaitMs)
+        {
+            await Task.Delay(intervalMs);
+            totalWait += intervalMs;
+        }
+    }
+
+    /// <summary>
+    /// Repeatedly check is the maximum wait time is reached.
+    /// </summary>
+    /// <param name="maxWaitMs">Maximum time to wait in milliseconds.</param>
+    /// <param name="intervalMs">Time to wait between checks in milliseconds.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    public static async Task UntilTrue(int maxWaitMs = 5000, int intervalMs = 100)
+    {
+        int totalWait = 0;
+        
+        // We want to loop AS LONG AS the condition is FALSE and we haven't timed out
+        while (totalWait < maxWaitMs)
         {
             await Task.Delay(intervalMs);
             totalWait += intervalMs;
