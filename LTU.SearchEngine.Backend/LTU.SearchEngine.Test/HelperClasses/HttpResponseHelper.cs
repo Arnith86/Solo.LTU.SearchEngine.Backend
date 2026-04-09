8c30d26a8d07b377d6334aa89776338502898182
Jean-Paul Hanna <101112377+Arnith86@users.noreply.github.com>
2026-04-09 11:15:56 +0200

    Update HttpResponseHelper.cs
    Can now also set content type and encoding type

--- LTU.SearchEngine.Backend/LTU.SearchEngine.Test/HelperClasses/HttpResponseHelper.cs
+++ LTU.SearchEngine.Backend/LTU.SearchEngine.Test/HelperClasses/HttpResponseHelper.cs
@@ -1,4 +1,5 @@
 using System.Net;
+using System.Net.Http.Headers;
 using Moq;
 using Moq.Protected;
 
@@ -6,19 +7,28 @@ namespace LTU.SearchEngine.Test.HelperClasses;
 
 public static class HttpResponseHelper
 {
-    public static void SetupHttpResponse(Mock<HttpMessageHandler> handlerMock, HttpStatusCode code, string content)
+    public static void SetupHttpResponse(
+        Mock<HttpMessageHandler> httpMessageHandlerMock, 
+        HttpStatusCode httpStatusCode, 
+        string content,
+        string contentType = "text/html",
+        string charSet = "utf-8")
     {
-        handlerMock
+        var response = new HttpResponseMessage
+        {
+            StatusCode = httpStatusCode,
+            Content = new StringContent(content, System.Text.Encoding.GetEncoding(charSet), contentType)
+        };
+
+        response.Content.Headers.ContentType = new MediaTypeHeaderValue(contentType){ CharSet = charSet };
+
+        httpMessageHandlerMock
             .Protected()
             .Setup<Task<HttpResponseMessage>>(
                 "SendAsync",
                 ItExpr.IsAny<HttpRequestMessage>(),
                 ItExpr.IsAny<CancellationToken>()
             )
-            .ReturnsAsync(new HttpResponseMessage
-            {
-                StatusCode = code,
-                Content = new StringContent(content, System.Text.Encoding.UTF8, "text/html")
-            });
+            .ReturnsAsync(response);
     }
 }
