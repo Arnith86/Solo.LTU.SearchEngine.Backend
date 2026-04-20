512758278c4fd41e33a627e83fae39c427a37e6d
Jean-Paul Hanna <101112377+Arnith86@users.noreply.github.com>
2026-04-20 08:00:58 +0200

    Added forgotten aspects of the entities (and fixed typo)

--- LTU.SearchEngine.Backend/LTU.SearchEngine.Backend.Core/Model/Entities/HtmlMetaData.cs
+++ LTU.SearchEngine.Backend/LTU.SearchEngine.Backend.Core/Model/Entities/HtmlMetaData.cs
@@ -1,7 +1,11 @@
+using LTU.SearchEngine.Backend.Core.Entities;
+
 namespace LTU.SearchEngine.Backend.Core.Model.Entities;
 
-public class HtmlMetaDate
+public class HtmlMetaData
 {
+    public int PageId { get; set; }
     public string CharSet { get; set; } = string.Empty;
     public string Doctype { get; set; } = string.Empty;
+    public Page Page { get; set; } = null!;  
 }
