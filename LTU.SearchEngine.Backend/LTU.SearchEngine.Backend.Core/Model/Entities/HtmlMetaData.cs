fc09e33719c0507667cf94b5358dae0b428bf1b6
Jean-Paul Hanna <101112377+Arnith86@users.noreply.github.com>
2026-04-20 07:27:58 +0200

    Created two new entities, Html and PdfMetaData

---
+++ LTU.SearchEngine.Backend/LTU.SearchEngine.Backend.Core/Model/Entities/HtmlMetaData.cs
@@ -0,0 +1,7 @@
+namespace LTU.SearchEngine.Backend.Core.Model.Entities;
+
+public class HtmlMetaDate
+{
+    public string CharSet { get; set; } = string.Empty;
+    public string Doctype { get; set; } = string.Empty;
+}
---
+++ LTU.SearchEngine.Backend/LTU.SearchEngine.Backend.Core/Model/Entities/PdfMetaData.cs
@@ -0,0 +1,7 @@
+namespace LTU.SearchEngine.Backend.Core.Model.Entities;
+
+public class PdfMetaDate
+{
+    public string PdfVersion { get; set; } = string.Empty;
+    public string EncodingType { get; set; } = string.Empty;
+}
